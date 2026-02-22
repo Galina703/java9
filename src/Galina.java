cb405da671e52506eca319d7da541c6f3a0bf753
Galina Bunu <galinabunu26@gmail.com>
2026-02-22 18:14:07 +0100

    Add salut

--- src/Galina.java
+++ src/Galina.java
@@ -1,6 +1,10 @@
+import java.io.PrintStream;
+
 public class Galina {
     static void main() {
         System.out.println("Hello world");
+        System.out.println("Salut");
+        System.out.print("Proiecte saptamina viitoare");
 
 
     }
